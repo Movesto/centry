00622ac9c61432e8f567199aecb98ff0869379f6
your_name <mmursal@sourceU.local>
2019-11-25 21:51:07 -0600

    add workshop project to git

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+alert("Hello World!");
+$(document).ready(function() {
+    $("#goal-button").click(function() {
+      $("#goal-list").append("<li class='list-item'>" + $("#new-goal").val() + "</li>");
+      $("#new-goal").val("");
+    });
+  });
+  $("#css-item").text()
+ 
